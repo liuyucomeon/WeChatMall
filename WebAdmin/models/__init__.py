ad3ab76ae8c7d845abaaa4a9991ef24f5c48e153
liuyu <15652294272@163.com>
2017-11-20 10:54:42 +0800

    ..

--- WebAdmin/models/__init__.py
+++ WebAdmin/models/__init__.py
@@ -1,2 +1,6 @@
 from .hotel import *
 from .staff import *
+from .commodity import *
+from .customer import *
+from .menu import *
+from .news import *
--- WebAdmin/models/customer.py
+++ WebAdmin/models/customer.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Customer(models.Model):
     name = models.CharField(max_length=20, blank=True, default='', help_text="名字")
-    phone = models.CharField(unique=True, help_text="手机号")
+    phone = models.CharField(max_length=20, unique=True, help_text="手机号")
     birthday = models.DateTimeField(default=None, null=True, help_text="生日")
     id_number = models.CharField(max_length=18, blank=True, default='', help_text="身份证号")
     gender = models.IntegerField(choices=((1, '男'), (2, '女')),
