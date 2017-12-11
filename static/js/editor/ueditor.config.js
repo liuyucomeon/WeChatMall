97645f3b4142fb02dee6a73224852d77c186e427
liuyu <15652294272@163.com>
2017-12-11 13:52:37 +0800

    ..

--- static/js/editor/ueditor.config.js
+++ static/js/editor/ueditor.config.js
@@ -30,7 +30,8 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl:  "http://114.215.220.241/WebAdmin/UEhandler/"
+        // , serverUrl:  "http://114.215.220.241/WebAdmin/UEhandler/"
+		, serverUrl:  "http://127.0.0.1/WebAdmin/UEhandler/"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
